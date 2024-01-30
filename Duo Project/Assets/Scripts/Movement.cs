032ed39984d40276c706f3e8e55474fc31c3f394
GameGeek <game0geek2020@gmail.com>
2024-01-30 18:42:57 +0400

    movement

--- Duo Project/Assets/Scripts/Movement.cs
+++ Duo Project/Assets/Scripts/Movement.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody2D rb;
+    private float swiftness =3;
+    private bool facingRight;
     // Start is called before the first frame update
     void Start()
     {
-        
+       rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //movements
+        float movementy = Input.GetAxis("Vertical");
+        float movementx = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(movementx * swiftness, movementy * swiftness);
+
+        //facingDirection
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            facingRight = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            facingRight = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.A)&& Input.GetKeyDown(KeyCode.D))
+        {
+            facingRight = false;
+        }
+        if(facingRight) { transform.localScale = new Vector3(-1, 1, 1); }
+        else { transform.localScale = new Vector3(1, 1, 1); }
+
     }
 }
